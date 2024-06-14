3168b8652564a33606be6f3772de31001917d170
PudCheetah <wer212131@gmail.com>
2024-06-14 22:47:01 +0800

    優化及DeBug

--- app/src/main/java/com/example/myquestquiz_1/QuestPageActivity/QuestPage_AlertDialogSet.kt
+++ app/src/main/java/com/example/myquestquiz_1/QuestPageActivity/QuestPage_AlertDialogSet.kt
@@ -14,7 +14,7 @@ class QuestPage_AlertDialogSet(var context: Context, var myVM: QuestPageViewMode
             .setTitle("確認視窗")
             .setMessage("即將跳轉到結果頁面  \n總題數: ${myVM.numExpect_intent.value}\n答對題數: ${myVM.scoreCounter.value!!.toInt()} \n答錯或跳過題數: ${(myVM.numExpect_intent.value!! - myVM.scoreCounter.value!!).toInt()} \n答題率: ${String.format("%.2f", (myVM.scoreCounter.value!!.toFloat() / myVM.numExpect_intent.value!!.toFloat()) * 100)}%")
             .setPositiveButton("跳轉", DialogInterface.OnClickListener { dialog, which ->
-                context.startActivity(intentManager.getIntent("ToResult"))
+                context.startActivity(intentManager.getIntent("ToSetting"))
             }).show()
     }
 }
